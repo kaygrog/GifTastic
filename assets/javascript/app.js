b0eb16d1783d50b930ef8832748b5023e576974b
kaygrog <kaylagrogan23@gmail.com>
2020-04-08 14:45:01 -0500

    Create draft html and Add css and js files

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+var topics = ["banana", "apple", "peach", "pear", "kiwi", "grape", "orange",
+              "plantain", "raspberry", "blueberry", "strawberry", "passionfruit",
+              "mango", "guava", "pomegranate", "clementine", "tangerine",
+              "cranberry", "dragonfruit", "starfruit", "lemon"]
+
