801b968a78f99a652ef45c6dcebeb4143a03727e
kaygrog <kaylagrogan23@gmail.com>
2020-04-12 21:16:51 -0500

    Add search functionality and ratings

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,26 +3,43 @@ var topics = ["banana", "apple", "peach", "pear", "kiwi", "grape", "orange",
               "french silk pie", "papaya", "white claw", "lacroix", "pizza",
               "sweet potato", "baja blast", "taco bell", "watermelon"]
 
-// Add food buttons to page
+// For each string in topics array, call function to create button and add it to buttons div
 for (var i=0; i < topics.length; i++) {
-    $("#buttons").append("<button type='button' class='btn btn-info food-button' id='" + topics[i] + "'>" + topics[i] + "</button>");
+    createButton(topics[i]);
+}
+
+// When the Submit button is clicked, add the topic in the form field to the array of buttons
+$("#submit-button").on("click", function(event) {
+
+    // Prevent page from refreshing upon button click
+    event.preventDefault();
+
+    // Call function to create button and add it to buttons div
+    createButton($("#topic-search").val());
+});
+
+// Add a button to the buttons div
+function createButton(topic) {
+    $("#buttons").append("<button type='button' class='btn btn-info food-button' id='" + topic + "'>" + topic + "</button>");
 }
 
 // When a food button is clicked, call the Giphy API and search for gifs using the button id as the search query
-$(".food-button").on("click", function() {
+$(document).on("click", ".food-button", function() {
+
+    // Prevent page from refreshing upon button click
+    event.preventDefault();
     
     // Clear gifs div
     $("#gifs").text("");
 
     // Create the url to use for the Giphy API call using the button id as the search query
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=scg3lg7S0jY6WEdDMnDymRFBHblIReU8&q=" + this.id + "&limit=10&rating=r";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=scg3lg7S0jY6WEdDMnDymRFBHblIReU8&q=" + this.id + "&limit=10";
 
     // Giphy API call
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        console.log(response.data[0].images.fixed_height_still.url);
         console.log(response);
 
         // Store the array of data from the Giphy API call resposne
@@ -30,12 +47,24 @@ $(".food-button").on("click", function() {
 
         // Loop through the array and add the images to the page
         for (var i=0; i < gifs.length; i++) {
+
+            // Grab data from Giphy API call response
             var stillURL = response.data[i].images.fixed_width_still.url;
             var animateURL = response.data[i].images.fixed_width.url;
+            var rating = response.data[i].rating;
 
-            // Add images to gifs div
+            // Create div to hold rating + gif
+            var gifContainer = $("<div class='gif-container'>");
+
+            // Append gif rating to div
+            gifContainer.append("<p class='rating'>Rating: " + rating + "</p>");
+
+            // Append gif to div
             // Make sure still version, animated version, and state of image are stored as attributes
-            $("#gifs").append("<img class='food-pic' src='" + stillURL + "' data-still='" + stillURL + "' data-animate='" + animateURL + "' data-state='still'>");
+            gifContainer.append("<img class='food-pic' src='" + stillURL + "' data-still='" + stillURL + "' data-animate='" + animateURL + "' data-state='still'>")
+
+            // Add div to gifs div in html
+            $("#gifs").append(gifContainer);
         }
 
         // When an image is clicked, call function to either animate or make still
